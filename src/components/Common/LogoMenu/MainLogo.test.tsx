e5acec91de06b3cdd1dcbf3b28f06134ef8ad10b
조은솔 <escho@inthecore.co.kr>
2019-10-07 17:33:14 +0900

    테스트(강제성공)

--- src/components/Common/LogoMenu/MainLogo.test.tsx
+++ src/components/Common/LogoMenu/MainLogo.test.tsx
@@ -3,7 +3,7 @@ import MainLogo from "./MainLogo";
 import renderer from 'react-test-renderer'
 
 test('MainLogo create', ()=>{
-   const component = renderer.create(<MainLogo userName="Trㅁacy"/>);
+   const component = renderer.create(<MainLogo userName="Tracy"/>);
    let tree = component.toJSON();
    expect(tree).toMatchSnapshot();
 });
